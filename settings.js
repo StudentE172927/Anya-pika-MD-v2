bd4eef0af6cfb0c1185d18da3b1ba72c12406b03
StudentE172927 <114796793+StudentE172927@users.noreply.github.com>
2023-02-11 13:52:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,27 +2,27 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "Queen Anya" 
+global.botname = "Queen anya" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
 global.author = "𝙋𝙄𝙆𝘼"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
+global.myweb = "https://github.com/StudentE172927/Anya-pika-MD-v2" // You can put any link 
+global.waterMark = "×͜× King ✘ Panda™ ✓" // Your own watermark 
+global.headerText = "Anya by Panda" // text for header of menu         
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
-global.ytname = "YT: Ytname"   // Yt chanel name //
+global.ownername = "PanDa'H  CroFT"   // Owner name //        
+global.ownernumber = ['94763476174'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : APX PANDA" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ytname = "YT: SF2SHADES"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
+global.region = "Sri Lanka" // your country name
+global.state = "Avissawella" // your state name
+global.district = "Kegalle" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
-global.email = "example@exmaple.com" // put your email here
+global.instagramId = "APX PANDA" // Your insta ID
+global.email = "chanmithudhan2008@gmail.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
